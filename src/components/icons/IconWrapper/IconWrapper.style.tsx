be6d84550e4069345b4eda0b1cf848ef8d714e76
sumini0829 <sumini0829@estsoft.com>
2023-03-09 21:25:56 +0900

    [fix]: width, height 믹스인으로 추가

--- src/components/icons/IconWrapper/IconWrapper.style.tsx
+++ src/components/icons/IconWrapper/IconWrapper.style.tsx
@@ -1,11 +1,11 @@
+import { size } from '@Styles/mixin.style';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div<{ width: string; height: string }>`
   margin: 0;
   padding: 0;
   cursor: pointer;
-  width: ${props => props.width};
-  height: ${props => props.height};
+  ${props => size(props.width, props.height)}
   & img {
     width: 100%;
     height: 100%;
